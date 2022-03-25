1efc850e1f895bc098897f5c9be687a1c8028cfc
mitsuru_yamaura <komayou326@gmail.com>
2022-03-25 14:24:38 +0900

    EnemyData.cs 作成。Bullet.cs 修正し、弾を当てた際のダメージ処理と破壊処理の追加。 エネミーのアニメーション同期の追加。

--- Assets/Scripts/EnemyBase.cs
+++ Assets/Scripts/EnemyBase.cs
@@ -7,16 +7,44 @@ using DG.Tweening;
 public class EnemyBase : MonoBehaviour
 {
     private Rigidbody rb;
+    private Animator anim;
+    private CapsuleCollider capsuleCollider;
+
+    private EnemyData enemyData;
 
     void Start()
     {
-        if(TryGetComponent(out rb)){
+        enemyData = new EnemyData { no = 0, hp = 2, attackPower = 1, moveSpeed = 3.5f };
+        TryGetComponent(out anim);
+        TryGetComponent(out capsuleCollider);
+        if (TryGetComponent(out rb)){
             MoveEnemy();
-        }
+        } 
     }
 
     
     private void MoveEnemy() {
-        rb.DOMoveX(Random.Range(5, 10), 1.5f).SetEase(Ease.InQuart).OnComplete(() => MoveEnemy());
+        rb.DOMoveX(transform.position.x + Random.Range(2, 4), enemyData.moveSpeed)
+            .SetLink(gameObject)
+            .SetEase(Ease.InQuart)
+            .OnComplete(() => { if (enemyData.hp > 0) MoveEnemy(); });
+        anim.SetBool("Walk Forward", true);
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.TryGetComponent(out Bullet bullet)) {
+            enemyData.hp = Mathf.Max(0, enemyData.hp -= bullet.BulletPower);
+            Debug.Log(enemyData.hp);
+
+            if (enemyData.hp <= 0) {
+                capsuleCollider.enabled = false;
+                rb.isKinematic = true;
+                anim.SetBool("Walk Forward", false);
+                anim.SetBool("Down", true);
+                Destroy(gameObject, 1.5f);
+                Debug.Log("Destroy");
+            }
+        }
     }
 }
