02b553d92c755c4a562f9852735cdd33b99cfb80
mitsuru_yamaura <komayou326@gmail.com>
2022-04-08 09:23:54 +0900

    AnyState 利用時と通常遷移の両方での連続攻撃機能を実装。デバッグ完了。

--- Assets/ContinuityAttackBehaviour.cs
+++ Assets/ContinuityAttackBehaviour.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Unityのアクションゲームで連続攻撃を実現する方法
+// https://gametukurikata.com/program/continuityattack
+
+// 【Unity】AnimationControllerのAnyStateを使用してる際、現在のStateへ何度も移動しないようにする
+// https://tsubakit1.hateblo.jp/entry/2017/01/13/233000
+
 public class ContinuityAttackBehaviour : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
@@ -11,19 +17,19 @@ public class ContinuityAttackBehaviour : StateMachineBehaviour
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        Debug.Log("OnStateUpdate" + layerIndex);
+        //Debug.Log("OnStateUpdate" + layerIndex);
 
         // AnyState を利用している場合は、Can Transition To Self にチェックが入っていると常に最初に戻ってしまうのでチェックを外す
         // AnyState を利用していない場合には特に問題なし
         if (Input.GetButtonDown("Fire1")) {
             animator.SetTrigger("Attack");
-            Debug.Log("ContinuousAttack");
+            Debug.Log("ContinuousAttack : " + layerIndex);
         }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        Debug.Log("OnStateExit" + layerIndex);
+        //Debug.Log("OnStateExit" + layerIndex);
         if (stateInfo.IsName("ContinuousAttack3")) {
             animator.ResetTrigger("Attack");
         }
